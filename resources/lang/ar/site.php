725270077b124d8dd8d5a7cf71fe2c0d993b9633
aliharby12 <aliharby565@gmail.com>
2020-08-30 11:01:00 -0700

    handle the create user functionlity

--- resources/lang/ar/site.php
+++ resources/lang/ar/site.php
@@ -8,6 +8,13 @@ return [
   'action' => 'الاكشن',
   'edit' => 'تعديل',
   'delete' => 'حذف',
+  'add' => 'اضف',
+  'search' => 'بحث',
   'no_data_found' => 'لا يوجد اي مشرفين حتى الان',
+  'password' => 'كلمه المرور',
+  'password_confirmation' => 'تاكيد كلمه المرور',
+  'add_successfully' => 'تم الاضافه بنجاح',
+  'updated_successfully' => 'تم الاضافه بنجاح',
+  'deleted_successfully' => 'تم الاضافه بنجاح',
 
 ];
--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -18,11 +18,31 @@
 
             <div class="box box-primary">
 
+                <div class="box-header with-border">
 
+
+                    <form action="{{ route('dashboard.users.index') }}" method="get">
+
+                        <div class="row">
+
+                            <div class="col-md-4">
+                                <input type="text" name="search" class="form-control" placeholder="@lang('site.search')" value="{{ request()->search }}">
+                            </div>
+
+                            <div class="col-md-4">
+                                <button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> @lang('site.search')</button>
+                                <a href="{{ route('dashboard.users.create') }}" class="btn btn-primary"><i class="fa fa-plus"></i> @lang('site.add')</a>
+                            </div>
+
+                        </div>
+                    </form><!-- end of form -->
+
+                </div><!-- end of box header -->
+                
                 <div class="box-body">
 
                     @if ($users->count() > 0)
-                      
+
                         <table class="table table-hover">
 
                             <thead>
@@ -42,7 +62,16 @@
                                     <td>{{ $user->first_name }}</td>
                                     <td>{{ $user->last_name }}</td>
                                     <td>{{ $user->email }}</td>
+                                    <td>
+                                        <a href="{{ route('dashboard.users.edit', $user->id) }}" class="btn btn-info btn-sm"><i class="fa fa-edit"></i> @lang('site.edit')</a>
 
+                                        <form action="{{ route('dashboard.users.destroy', $user->id) }}" method="post" style="display: inline-block">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger delete btn-sm"><i class="fa fa-trash"></i> @lang('site.delete')</button>
+                                        </form><!-- end of form -->
+
+                                    </td>
                                 </tr>
 
                             @endforeach
