207816f8dc5df8bd3825e34d1a8de67bf88682f9
aliharby12 <aliharby565@gmail.com>
2020-09-08 07:58:01 -0700

    integrate the site template with it's assets

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes(['register' => false ]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function(){
+    return view('site.index');
+});
